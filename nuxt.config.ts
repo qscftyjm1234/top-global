685d1f581c399a6e86f57ff606f2666afbde943d
gino.huang <gino.huang@kjsoft.com.tw>
2024-04-02 14:10:13 +0800

    換路徑

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2024-04-02 13:07:08
  * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2024-04-02 13:52:45
+ * @LastEditTime: 2024-04-02 14:07:30
  * @FilePath: \top-glob\nuxt.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,9 +10,13 @@
 const path = require('path');
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  // 輸出後更改資料夾
   nitro: {
     output: {
       publicDir: path.join(__dirname, 'docs')
     },
+  },
+  app: {
+    baseURL: '/nuxt-github-pages/',
   },
 })
