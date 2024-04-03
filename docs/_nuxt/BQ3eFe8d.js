aa88af5f5958484e7152000cf3542e4e0ab58ff9
gino.huang <gino.huang@kjsoft.com.tw>
2024-04-03 10:44:44 +0800

    部屬

---
+++ docs/_nuxt/BQ3eFe8d.js
@@ -0,0 +1,1 @@
+import{_ as e,v as r,t as c}from"./7K2ZRu-4.js";const t={};function n(s,o){return c(),r("div",null," server ")}const a=e(t,[["render",n]]);export{a as default};
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2024-04-02 13:07:08
  * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2024-04-03 10:24:06
+ * @LastEditTime: 2024-04-03 10:43:19
  * @FilePath: \top-glob\nuxt.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,7 +14,6 @@ import { resolve } from 'pathe';
 // import Components from 'unplugin-vue-components/vite';
 // import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 export default defineNuxtConfig({
-  ssr: false,
   devtools: { enabled: true },
   // 輸出後更改資料夾
   nitro: {
