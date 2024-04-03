ced0c81a39b52fafeed1edb2793cefc58b8cb677
gino.huang <gino.huang@kjsoft.com.tw>
2024-04-03 09:05:21 +0800

    部屬

--- docs/_nuxt/D7TNZdXM.js
+++
@@ -1,1 +0,0 @@
-import{_ as n,c as _,d as c,b as o,o as a,as as r}from"./h555ljCn.js";import{_ as s}from"./CHZmJtZc.js";const m={};function p(u,d){const e=r,t=s;return a(),_("div",null,[c(" 進入點2 "),o(e),o(t)])}const x=n(m,[["render",p]]);export{x as default};
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2024-04-02 13:07:08
  * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2024-04-02 17:41:54
+ * @LastEditTime: 2024-04-03 09:02:59
  * @FilePath: \top-glob\nuxt.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -17,22 +17,18 @@ export default defineNuxtConfig({
       publicDir: path.join(__dirname, 'docs')
     },
   },
-  router: {
-    base: '/nuxt-github-pages/',
-  },
   app: {
-    baseURL: '/nuxt-github-pages/',
-    // head: {
-    //   meta: [{ name: '邏捷運通', content: '邏捷運通' }],
-    // },
+    baseURL: '/top-global/',
+    head: {
+      meta: [{ name: '邏捷運通', content: '邏捷運通' }],
+    },
+  }, 
+  router: {
+    base: '/top-global/',
   },
   modules: [
     '@element-plus/nuxt'
   ],
-  elementPlus: {
-    // themes: ["dart"]
-    themes: ["dark"]
-  },
   // elementPlus: { /** Options */ }
   css: ['element-plus/dist/index.css', 'assets/styles/index.scss'],
 	alias: {
