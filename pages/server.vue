00874ce2a1d6a0bd21c889bfb4ba990b304d78a4
qscftyjm1234 <qscftyjm1234@gmail.com>
2024-04-10 10:15:31 +0800

    部屬

--- pages/server.vue
+++ pages/server.vue
@@ -26,19 +26,155 @@
                         @tab-click="handleClick"
                     >
                         <el-tab-pane label="進口" name="1">
-                            
+                            <section>
+                                <div class="content-title">一、服務國家</div>
+                                <el-tag
+                                    type="primary"
+                                    effect="dark"
+                                    class="tag"
+                                    size="large"
+                                >
+                                    越南
+                                </el-tag>
+                                <el-tag
+                                    type="primary"
+                                    effect="dark"
+                                    class="tag"
+                                    size="large"
+                                >
+                                    印尼
+                                </el-tag>
+                                <el-tag
+                                    type="primary"
+                                    effect="dark"
+                                    class="tag"
+                                    size="large"
+                                >
+                                    泰國
+                                </el-tag>
+                                <!-- <el-check-tag type="primary" checked="true" class="tag">
+                                    越南
+                                </el-check-tag>
+                                <el-check-tag type="primary" checked="true" class="tag">
+                                    印尼
+                                </el-check-tag>
+                                <el-check-tag type="primary" checked="true" class="tag">
+                                    泰國
+                                </el-check-tag> -->
+                             </section>
                         </el-tab-pane>
                         <el-tab-pane label="出口" name="2">
-                            
+                            <section>
+                                <div class="content-title">一、服務國家</div>
+                                <el-tag
+                                    type="primary"
+                                    effect="dark"
+                                    class="tag"
+                                    size="large"
+                                >
+                                    越南
+                                </el-tag>
+                                <el-tag
+                                    type="primary"
+                                    effect="dark"
+                                    class="tag"
+                                    size="large"
+                                >
+                                    印尼
+                                </el-tag>
+                                <el-tag
+                                    type="primary"
+                                    effect="dark"
+                                    class="tag"
+                                    size="large"
+                                >
+                                    泰國
+                                </el-tag>
+                                <el-tag
+                                    type="primary"
+                                    effect="dark"
+                                    class="tag"
+                                    size="large"
+                                >
+                                    菲律賓
+                                </el-tag>
+                                <el-tag
+                                    type="primary"
+                                    effect="dark"
+                                    class="tag"
+                                    size="large"
+                                >
+                                    美國(重貨21KG以上)
+                                </el-tag>
+                             </section>
                         </el-tab-pane>
-                        <el-tab-pane label="集運" name="3">
-                            
+                        <el-tab-pane label="集貨進口" name="3">
+                            <section>
+                                <div class="content-title">一、服務國家</div>
+                                <el-tag
+                                    type="primary"
+                                    effect="dark"
+                                    class="tag"
+                                    size="large"
+                                >
+                                    大陸
+                                </el-tag>
+                                <el-tag
+                                    type="primary"
+                                    effect="dark"
+                                    class="tag"
+                                    size="large"
+                                >
+                                    日本
+                                </el-tag>
+                             </section>
                         </el-tab-pane>
                         <el-tab-pane label="費用" name="4">
                             
                         </el-tab-pane>
+                        <el-tab-pane label="快遞" name="6">
+                            <section>
+                                <div class="content-title">一、體積(材積)計算方式</div>
+                                <p>
+                                    內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文
+                                </p>
+                                <hr />
+                                <div class="content-title">二、重量計算方式</div>
+                                <p>
+                                    內文內文內文內文內文內文內文內文內文內文內文
+                                </p>
+                             </section>
+                        </el-tab-pane>
+                        <el-tab-pane label="海運" name="7">
+                            <section>
+                                <div class="content-title">一、體積(材積)計算方式</div>
+                                <p>
+                                    內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文
+                                </p>
+                                <hr />
+                                <div class="content-title">二、重量計算方式</div>
+                                <p>
+                                    內文內文內文內文內文內文內文內文內文內文內文
+                                </p>
+                            </section>
+                        </el-tab-pane>
+                        <el-tab-pane label="EMS 快遞" name="8">
+                            <section>
+                                <div class="content-title">一、體積(材積)計算方式</div>
+                                <p>
+                                    內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文內文
+                                </p>
+                                <hr />
+                                <div class="content-title">二、重量計算方式</div>
+                                <p>
+                                    內文內文內文內文內文內文內文內文內文內文內文
+                                </p>
+                            </section>
+                        </el-tab-pane>
                         <el-tab-pane label="加值費用" name="5">
-                            
+                            <section>
+
+                            </section>
                         </el-tab-pane>
                     </el-tabs>
                 </el-col>
@@ -55,13 +191,18 @@
 section {
     padding: 36px 0px;
 }
+.content-title {
+    font-size: 28px;
+    color: rgb(32, 151, 219);
+    margin-bottom: 24px;
+}
+
 .title {
     display: flex;
     align-items: center;
     line-height: 42px;
     font-size: 24px;
     font-weight: bold;
-    color: rgb(32, 151, 219);
     margin-bottom: 24px;
 }
 
@@ -79,6 +220,12 @@ section {
   color: #6b778c;
   font-size: 32px;
   font-weight: 600;
+}
+hr {
+    margin: 24px 0px;
+}
+.tag {
+    margin-right: 12px;
 }
 
 </style>
