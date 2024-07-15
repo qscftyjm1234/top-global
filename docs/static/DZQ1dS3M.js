8c76ace2bf75006bfa6cc8034b2e25b30b120d15
unknown <gino.huang@kjsoft.com.tw>
2024-07-15 17:30:25 +0800

    gogo

--- docs/static/DZQ1dS3M.js
+++
@@ -1,1 +0,0 @@
-import"./C5WOM-2S.js";const r=""+new URL("logo.BsJd-_cG.jpg",import.meta.url).href;export{r as _};
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,23 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2024-04-02 13:07:08
  * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2024-07-15 17:18:23
+ * @LastEditTime: 2024-07-15 17:29:05
  * @FilePath: \top-glob\nuxt.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 const path = require('path');
 
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/top-global/', // 配置應用程序的基本 API 請求 URL
+    base: '/top-global/',
+    buildAssetsDir: '/static/',
+  },
+  router: {
+    base: '/top-global/',
+  },
+  
   server: {
-    // 代理設置
     proxy: {
       '/api/': {
         target: 'http://jingya.api.8bami.com',
@@ -19,14 +27,6 @@ export default defineNuxtConfig({
         secure: false
       }
     }
-  },
-  app: {
-    baseURL: '/top-global/', // 配置應用程序的基本 API 請求 URL
-    base: '/top-global/',
-    buildAssetsDir: '/static/',
-  },
-  router: {
-    base: '/top-global/',
   },
   devtools: { enabled: true },
   modules: [
